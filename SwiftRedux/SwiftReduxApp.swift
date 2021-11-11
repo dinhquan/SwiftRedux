36be8aafafcf612ce4e435c1bf28c24c3b68172f
Quan <dinhquan191@gmail.com>
2021-11-11 15:08:37 +0700

    First commit

---
+++ SwiftRedux/SwiftReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftReduxApp.swift
+//  SwiftRedux
+//
+//  Created by Quan on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
